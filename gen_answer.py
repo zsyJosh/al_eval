d963fe485abadc61dd3bdcd072db55b01fe976bb
zsyJosh <61644849+zsyJosh@users.noreply.github.com>
2023-07-26 17:58:46 +0800

    Update gen_answer.py

--- gen_answer.py
+++ gen_answer.py
@@ -2,13 +2,20 @@ import datasets
 from fastchat.model.model_adapter import load_model
 import os
 import argparse
+import json
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--model-path",
         type=str,
-        required=True)
+        required=True
+    )
+    parser.add_argument(
+        "--ans-path",
+        type=str,
+        help="directory to store then answer.",
+    )
     parser.add_argument(
         "--num-gpus-per-model",
         type=int,
@@ -22,11 +29,8 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    model_path = os.environ.get("MODEL_PATH", None)
-    assert model_path is not None
-
     model, tokenizer = load_model(
-            model_path,
+            args.model_path,
             device="cuda",
             num_gpus=args.num_gpus_per_model,
             max_gpu_memory=args.max_gpu_memory,
@@ -39,3 +43,8 @@ if __name__ == "__main__":
     eval_set = datasets.load_dataset("tatsu-lab/alpaca_eval", "alpaca_eval")["eval"]
     for example in eval_set:
         example["output"] = model.generate(example["instruction"])
+
+    ans_file = args.ans_path
+    with open(ans_file, 'w') as file:
+        json.dump(eval_set, file)
+    file.close()
